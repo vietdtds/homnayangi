16c2bc1e4fb310ade780d599b4e496d036586d44
VietDH <vietdtds@gmail.com>
2023-04-07 09:09:32 +0700

    update code

--- app/src/main/java/com/example/homnayangi/model/Meal.java
+++ app/src/main/java/com/example/homnayangi/model/Meal.java
@@ -1,15 +1,42 @@
 package com.example.homnayangi.model;
 
+import androidx.annotation.DrawableRes;
+
 import java.io.Serializable;
 
 public class Meal implements Serializable {
-    String thumbnail;
+    @DrawableRes
+    int thumbnail;
     String title;
     String description;
 
-    public Meal(String thumbnail, String title, String description) {
+    public Meal(@DrawableRes int thumbnail, String title, String description) {
         this.thumbnail = thumbnail;
         this.title = title;
+        this.description = description;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
